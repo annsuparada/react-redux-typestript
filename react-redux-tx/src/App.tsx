6e82d55f697bbae3cb93064160fecb5ff7fe36d1
Ann Keller <ann.keller@draft2digital.com>
2022-04-18 11:42:44 -0500

    dispatch actions in the App.js

--- react-redux-tx/src/App.tsx
+++ react-redux-tx/src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useDispatch, useSelector } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actionCreators, State } from "./state";
+import "./App.css"
 
 function App() {
+  const state = useSelector((state: State) => state.bank)
+  const dispatch = useDispatch();
+  const {depositMoney, withdrawMoney, bankrupt} = bindActionCreators(actionCreators, dispatch); 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>{state}</h1>
+        <button onClick={() => depositMoney(100)}>Deposit</button>
+        <button onClick={() => withdrawMoney(10)}>Withdraw</button>
+        <button onClick={() => bankrupt(0)}>Bankrupt</button>
     </div>
   );
 }
